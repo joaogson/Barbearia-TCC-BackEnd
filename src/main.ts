b5537c894fdf8b84977d13125a0b6b28723e7acb
joaogson <126979581+joaogson@users.noreply.github.com>
2025-11-11 18:24:54 -0300

    update url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app/app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://barbearia-arwhxgm0a-joao-sonalios-projects.vercel.app", // A URL do seu front (Vercel)
+    origin: "https://barbearia-tcc-front-nih3fvtl5-joao-sonalios-projects.vercel.app", // A URL do seu front (Vercel)
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
