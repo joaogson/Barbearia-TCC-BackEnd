ad484a02b4660dc4e838bff78ff6cdf1a166308f
joaogson <126979581+joaogson@users.noreply.github.com>
2025-11-11 18:02:16 -0300

    uptimeRobot update

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService
+  ) {}
+
+  @Get("/health") // <- O novo endpoint
+  async healthCheck() {
+    // Esta query é leve e força o banco a ficar acordado
+    await this.prisma.$queryRaw`SELECT 1`;
+
+    return { status: "ok", time: new Date() };
+  }
 
   @Get()
   getHello(): string {
