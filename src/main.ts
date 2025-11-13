f9e8f75201ce932558e51771826678f00f6ade97
joaogson <126979581+joaogson@users.noreply.github.com>
2025-11-13 00:14:14 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("America/Sao_Paulo");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
