98d36f98fb53b9db2a659b0b4fd6092ebdbb703a
João Gabriel M. Sonalio <126979581+joaogson@users.noreply.github.com>
2025-11-13 11:51:13 -0300

    update main.ts TIMEZONE

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import isBetween from "dayjs/plugin/isBetween";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 
+dayjs.extend(customParseFormat);
+dayjs.extend(isBetween);
 dayjs.extend(utc);
 dayjs.extend(timezone);
-dayjs.tz.setDefault("America/Sao_Paulo");
+
+// Defina o fuso padrão de TODO o back-end para o fuso da barbearia
+dayjs.tz.setDefault("America/Sao_Paulo"); 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
