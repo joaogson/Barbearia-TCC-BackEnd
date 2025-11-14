3efc79ff721972b7d010f1d4b8e5a557697ed208
joaogson <126979581+joaogson@users.noreply.github.com>
2025-11-13 23:53:23 -0300

    update CORS para dev

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL, // A URL do seu front (Vercel)
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"], // A URL do seu front (Vercel)
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
