d3d98499517e430e3193cb203cbd3b94344a9851
joaogson <126979581+joaogson@users.noreply.github.com>
2025-11-21 11:15:45 -0300

    update

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,7 +1,7 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateUserDto } from "./create-user.dto";
 import { IsEnum, IsNotEmpty, IsOptional, IsString, IsStrongPassword, Max, MaxLength } from "class-validator";
-
+import { Transform } from "class-transformer";
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsString()
@@ -17,5 +17,10 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsString()
   @MaxLength(64)
   @IsOptional()
+  @Transform(({ value }) => {
+    if (typeof value === "string") {
+      return value.replace(/\D/g, "");
+    }
+  })
   readonly phone?: string;
 }
