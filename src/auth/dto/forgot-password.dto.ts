816643aa76d02021fab359097acbe7ec77fddf19
joaogson <126979581+joaogson@users.noreply.github.com>
2025-11-27 00:04:30 -0300

    update forgot-password

--- src/auth/dto/forgot-password.dto.ts
+++ src/auth/dto/forgot-password.dto.ts
@@ -1,3 +1,7 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class ForgotPasswordDto {
+  @IsNotEmpty({ message: "O e-mail não pode ser vazio." })
+  @IsEmail({}, { message: "Forneça um endereço de e-mail válido." })
   email: string;
 }
